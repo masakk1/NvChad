0e6cd9cbe358dd339f4cda6bc39a2c48597c59ab
masakk1 <igotscammedlmfaobutimmasak1@gmail.com>
2023-04-15 16:27:29 +0200

    initial commit

---
+++ lua/custom/configs/lspconfig.lua
@@ -0,0 +1,17 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+
+-- if you just want default config for the servers then put them in a table
+local servers = { "html", "cssls", "tsserver", "lua_ls", "luau_lsp", "pyright" }
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+-- for a manual setup:
+-- lspconfig.pyright.setup { blabla }
---
+++ lua/custom/configs/overrides.lua
@@ -0,0 +1,70 @@
+local M = {}
+
+M.treesitter = {
+  ensure_installed = {
+    -- basic
+    "vim",
+    "lua",
+    "python",
+
+    -- web dev
+    "html",
+    "css",
+    "javascript",
+    "typescript",
+    "tsx",
+
+    -- extras
+    "c",
+    "markdown",
+    "markdown_inline",
+  },
+  indent = {
+    enable = true,
+    -- disable = {
+    --   "python"
+    -- },
+  },
+}
+
+M.mason = {
+  ensure_installed = {
+    -- lua stuff
+    "lua-language-server",
+    "stylua",
+
+    -- python
+    "pyright",
+    "ruff",
+    "black",
+
+    -- web dev stuff
+    "css-lsp",
+    "html-lsp",
+    "typescript-language-server",
+    "deno",
+    "prettier",
+
+    -- c/cpp stuff
+    "clangd",
+    "clang-format",
+  },
+}
+
+-- git support in nvimtree
+M.nvimtree = {
+  git = {
+    enable = true,
+  },
+
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+}
+
+return M
